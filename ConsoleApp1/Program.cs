721cdd6308db136b0e3665ee7a90e59668442da7
anur abubeker osman <abeare21@hotmail.com>
2019-08-19 17:54:46 +0300

    second

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.Write("hello");
+            Console.WriteLine("second change from vscode");
         }
     }
 }
